bb11b874ad6e07bb9402a34cb184e8571f408dc9
Liedha <115003614+Liedha@users.noreply.github.com>
2023-11-28 18:15:04 +0800

    Add files via upload

---
+++ PRAKTIKUM5/soal1/Shape.java
@@ -0,0 +1,21 @@
+package soal1;
+
+abstract class Shape {
+    protected String shapeName;
+
+    public Shape(String name) {
+        shapeName = name;
+    }
+
+    public abstract double area();
+
+    public String toString() {
+        return "Shape: " + shapeName;
+    }
+}
+
+
+
+
+
+
